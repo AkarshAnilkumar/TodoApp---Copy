a3d6bc46815b8e5ff71420c1349f7d179e1cf136
Akarsh <79101639+AkarshAnilkumar@users.noreply.github.com>
2024-04-28 11:31:29 +0530

    updated database 2

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,8 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "996bf76dae69e6da2cf1287d083f74a352c77b21"
-app.config["MONGO_URI"] = "mongodb+srv://abhinandmukundan15:<abhinandmukundan15>@cluster0.nttfi0k.mongodb.net/"
+# app.config["MONGO_URI"] = "mongodb+srv://abhinandmukundan15:<abhinandmukundan15>@cluster0.nttfi0k.mongodb.net/"
+app.config["MONGO_URI"] = "mongodb+srv://abhinandmukundan15:<password>@cluster0.nttfi0k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # app.config["MONGO_URI"]="mongodb://localhost:27017/todo"
 # mongodb database
