e3f2527bce4020afd86a69d45c41af86e5ff6079
Akarsh <79101639+AkarshAnilkumar@users.noreply.github.com>
2024-04-28 12:18:39 +0530

    8th time database

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from pymongo.mongo_client import MongoClient
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "996bf76dae69e6da2cf1287d083f74a352c77b21"
@@ -13,3 +14,12 @@ mongodb_client = PyMongo(app)
 db = mongodb_client.db
 
 from application import routes
+
+uri = "mongodb+srv://akarshkonniyoor:qoUj2D8ciQ1rbOGt@cluster0.jah2qxe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(uri)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
